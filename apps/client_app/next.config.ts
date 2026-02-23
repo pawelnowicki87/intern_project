a3160e754b98ec4800ccf8f016b5815688e359a9
Paweł Nowicki <pawel.nowicki87@tlen.pl>
2026-02-23 23:29:17 +0100

    changed api client

--- apps/client_app/next.config.ts
+++ apps/client_app/next.config.ts
@@ -8,31 +8,6 @@ loadEnv({
 const nextConfig = {
   reactStrictMode: true,
 
-  async rewrites() {
-    const isDev = process.env.NODE_ENV === 'development';
-
-    return [
-      {
-        source: '/api/auth/:path*',
-        destination: isDev
-          ? 'http://localhost:3002/:path*'
-          : 'http://16.16.94.166:3002/:path*',
-      },
-      {
-        source: '/api/core/:path*',
-        destination: isDev
-          ? 'http://localhost:3001/:path*'
-          : 'http://16.16.94.166:3001/:path*',
-      },
-      {
-        source: '/api/notifications/:path*',
-        destination: isDev
-          ? 'http://localhost:3003/:path*'
-          : 'http://16.16.94.166:3003/:path*',
-      },
-    ];
-  },
-
   env: {
     NEXT_PUBLIC_GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
   },
